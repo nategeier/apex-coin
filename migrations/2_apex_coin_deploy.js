4ef8a728efde8c62ca18e51ea8a35919933d4531
Nathanael Geier <nathanaelgeier@Nathanaels-MacBook-Pro.local>
2018-10-09 13:15:22 -0700

    Sorted, move on

--- migrations/2_apex_coin_deploy.js
+++ migrations/2_apex_coin_deploy.js
@@ -1,10 +1,7 @@
-const ApexCoin = artifacts.require('./ApexCoin.sol');
+const ApexCoin = artifacts.require("./ApexCoin.sol");
 
-const custodian = '0x592A714714A54Ff9387e8D1579e4b2878E49e212';
+const custodian = "0x592A714714A54Ff9387e8D1579e4b2878E49e212";
 
 module.exports = function(deployer) {
-  deployer.deploy(ApexCoin, 'Apex Coin', 'APX', 2);
-
-  // const apexCoin = await ApexCoin.deployed();
-  // console.log('this eRC20Store=======?', apexCoin.address);
+  deployer.deploy(ApexCoin, "Apex Coinz", "APX", 2);
 };
